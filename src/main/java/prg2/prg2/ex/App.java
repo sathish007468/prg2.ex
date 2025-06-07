b0c57ed5f59f2ed11c7356a30c2feff2d3131347
SATHISHG <satish@cambridge.edu.in>
2025-06-07 06:13:24 +0530

    Initial commit with Jenkinsfile

---
+++ src/main/java/prg2/prg2/ex/App.java
@@ -0,0 +1,13 @@
+package prg2.prg2.ex;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
